15d9690b8f1b3386a3dbdc450a282eb3fd92b80a
jessicashannon <jessdahling@gmail.com>
2015-07-22 13:03:24 -0400

    done

--- lib/003_methods.rb
+++ lib/003_methods.rb
@@ -1,2 +1,33 @@
-# code you methods for the 003_method spec here
-# Note: You will need to pass the school variable to each of these methods to include it in scope
+  school = {
+  :name => 'Happy Funtime School',
+  :location => 'NYC',
+  :instructors => [
+    {:name=>'Blake', :subject=>'being awesome' },
+    {:name=>'Steven', :subject=>'being better than blake'},
+    {:name=>'Jeff', :subject=>'karaoke'}
+  ],
+  :students => [
+    {:name => 'Marissa', :grade => 'B'},
+    {:name=>'Billy', :grade => 'F'},
+    {:name => 'Frank', :grade => 'A'},
+    {:name => 'Sophie', :grade => 'C'}
+  ]
+  }
+
+def get_grade(school, name)
+    school[:students].map{|student| student[:grade] if student[:name] == name}.compact.join
+end
+
+def update_subject(school, instructor, new_subject)
+  school[:instructors].each{
+    |instructors| instructors[:subject] = new_subject if instructors[:name] == instructor
+  }
+end
+
+def add_new_student(school, student, grade="nil")
+  school[:students].push({:name => student, :grade => grade})
+end
+
+def add_new_key(school, key, value)
+  school[key] = value
+end
